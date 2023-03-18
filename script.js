ab49c909e5eca06e7eb0b9299606755611e8b85b
sh1baruuu <114075679+sh1baruuu@users.noreply.github.com>
2023-03-18 15:15:06 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+
+window.addEventListener('blur', () =>{
+    document.title = "Come back here!";
+});
+window.addEventListener('focus', () =>{
+    document.title = "De Villa | Portfolio";
+});
+
+const navLinks = document.querySelectorAll('.link a');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        
+        const msg = "This section is under development. Please check back later!";
+        const targetId = this.getAttribute('href');
+        if (targetId=="#contacts"){
+            alert(msg);
+            alert("If you want to get in touch with me, please send an email to devillarudolphangelo@gmail.com.");
+        }else{
+            alert(msg);
+        }
+    });
+});
+
+const hamburgerBtn = document.querySelector('.hamburgerBtn');
+
+hamburgerBtn.addEventListener('click', () =>{
+    alert("This is under development. Please check back later!");
+});
